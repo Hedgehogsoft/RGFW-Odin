3d6ea66ff7b67cb1a03d1fdfbbab3767ac2a0cd8
ColleagueRiley <ColleagueRiley@gmail.com>
2026-06-17 23:57:20 -0400

    add build.bat

---
+++ build.bat
@@ -0,0 +1,38 @@
+@echo off
+
+if defined VSCMD_VER goto compile
+if defined env_set goto compile
+
+REM Check for Visual Studio 2022
+if not defined vs_path (
+    if exist "%ProgramFiles%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat" (
+        set "vs_path=%ProgramFiles%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
+    ) else (
+        REM Check for Visual Studio 2019
+        if exist "%ProgramFiles%\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat" (
+            set "vs_path=%ProgramFiles%\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+        ) else (
+            REM Check for Visual Studio 2017
+            if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat" (
+                set "vs_path=%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
+            ) else (
+                echo Visual Studio environment not found.
+                exit /b 1
+            )
+        )
+    )
+)
+
+if defined vs_path (
+    call "%vs_path%" x64
+    set "env_set=1"
+)
+
+:compile
+pushd RGFW
+if not exist lib mkdir lib
+echo Building libraries to RGFW/lib folder
+cl -nologo -MT -TC -c -O2 RGFW.c
+lib -nologo RGFW.obj -out:lib/RGFW_msvc.lib
+del *.obj
+popd
